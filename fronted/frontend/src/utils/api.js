6cbd50c4f9da94e85433d84f9d2700646d58de5d
Sunny kumar <sk5962779@gmail.com>
2026-03-07 03:54:06 +0530

    backend deploy

--- fronted/frontend/src/utils/api.js
+++ fronted/frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/auth"
+    baseURL: "compus360-production.up.railway.app"
 });
 
 // 🔥 Automatically token attach karega future me
